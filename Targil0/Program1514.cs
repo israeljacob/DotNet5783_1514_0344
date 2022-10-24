79418ecb1988649b5de587340f928dcb5cba795a
ישראל דזיקוב <ijacob@g.jct.ac.il>
2022-10-24 19:04:07 +0300

    first commit

---
+++ Targil0/Program1514.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Targil0
+{
+   partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1514();
+            Welcome0344();
+            Console.ReadKey();
+        }
+
+        private static void Welcome1514()
+        {
+            Console.WriteLine("Enter your name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+        static partial void Welcome0344();
+        
+    }
+}
