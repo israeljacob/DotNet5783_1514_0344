b01a9ee113839f17f4185b4431333e50879c29ca
ישראל דזיקוב <ijacob@g.jct.ac.il>
2022-11-01 11:19:14 +0200

    class config and more

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -4,12 +4,48 @@ namespace Dal;
 
 internal static class DataSource
 {
+    static int ID = 1000000;
     static Random r = new Random();
-    static readonly int RandomNumber= r.Next(0, 1000);
-
+    static readonly int RandomNumber = r.Next(0, 1000);
     internal static Order[] orders=new Order[100];
     internal static OrderItem[] orderItems = new OrderItem[200];
     internal static Product[] Products = new Product[50];
+
+    internal static class Config
+    {
+        
+        
+        private static int availableOrder=0;
+        internal static int AvailableID
+        {
+            get { return orderID++; }
+        }
+
+        private static int availableProduct=0;
+        internal static int AvailableProduct
+        {
+            get { return orderID++; }
+        }
+        private static int orderID = 1000000;
+        
+        internal static int OrderID
+        {
+            get { return orderID++; }
+        }
+
+
+        private static int productID = 2000000;
+        internal static int ProductID
+        {
+            get { return orderID++; }
+        }
+        private static int itemOrderID= 3000000;
+        internal static int ItemOrderID
+        {
+            get { return orderID++; }
+        }
+
+    }
 
 
     private static void  OrderInitial()
