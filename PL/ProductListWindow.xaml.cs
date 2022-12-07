d91344e35d3d019771cd778cedc31cca24190849
ישראל ג'יקוב <yisraeljakob@gmail.com>
2022-12-07 11:54:52 +0200

    hide buttons

--- PL/ProductListWindow.xaml.cs
+++ PL/ProductListWindow.xaml.cs
@@ -15,63 +15,62 @@ using System.Windows.Shapes;
 using BlImplementation;
 using System.Reflection;
 
-namespace PL
+namespace PL;
+
+/// <summary>
+/// Interaction logic for ProductListWindow.xaml
+/// </summary>
+public partial class ProductListWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for ProductListWindow.xaml
-    /// </summary>
-    public partial class ProductListWindow : Window
+    
+    private IBL bl = new Bl();
+    public ProductListWindow()
     {
-        
-        private IBL bl = new Bl();
-        public ProductListWindow()
-        {
-            InitializeComponent();
-            ProductListview.ItemsSource = bl.Product.GetListOfProducts();
-            CategorySelector.ItemsSource = Enum.GetValues(typeof(DO.Category));
-        }
-
-        private void CategorySelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if((DO.Category)CategorySelector.SelectedItem==DO.Category.all)
-            {
-                ProductListview.ItemsSource = bl.Product.GetListOfProducts();
-
-            }
-            else
-            {
-
-                Func<BO.ProductForList?, bool> func = product => product?.Category == (BO.Category)CategorySelector.SelectedItem;
-                ProductListview.ItemsSource = bl.Product.GetListOfProducts(func);
-            }
-        }
-
-        private void AddProdct_Click(object sender, RoutedEventArgs e)
-        {
-           new ProductWindow().Show();
-            this.Close();
-        }
-
-        private void ProductListview_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            BO.Product ourProduct = (BO.Product)ProductListview.SelectedItem;
-             if (ourProduct != null) { new ProductWindow().Show(); }
-        }
-
-        //private void ProductListview_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-
-        //}
-
-        //private void ProductListview_Selected(object sender, RoutedEventArgs e)
-        //{
-
-        //}
-
-        //private void ProductListview_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    BO.Product ourProduct = (Product)ProductListview.SelectedItem;
-        //   // if (ourProduct != null) { new ProductWindow().Show(); }
-        //}
+        InitializeComponent();
+        ProductListview.ItemsSource = bl.Product.GetListOfProducts();
+        CategorySelector.ItemsSource = Enum.GetValues(typeof(DO.Category));
     }
+
+    private void CategorySelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if((DO.Category)CategorySelector.SelectedItem==DO.Category.all)
+        {
+            ProductListview.ItemsSource = bl.Product.GetListOfProducts();
+
+        }
+        else
+        {
+            Func<BO.ProductForList?, bool> func = product => product?.Category == (BO.Category)CategorySelector.SelectedItem;
+            ProductListview.ItemsSource = bl.Product.GetListOfProducts(func);
+        }
+    }
+
+    private void AddProdct_Click(object sender, RoutedEventArgs e)
+    {
+       new ProductWindow(sender as Button).Show();
+        
+        this.Close();
+    }
+
+    private void ProductListview_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        BO.ProductForList ourProduct = (BO.ProductForList)ProductListview.SelectedItem;
+         if (ourProduct != null) { new ProductWindow(sender as Button).Show();}
+    }
+
+    //private void ProductListview_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    //{
+
+    //}
+
+    //private void ProductListview_Selected(object sender, RoutedEventArgs e)
+    //{
+
+    //}
+
+    //private void ProductListview_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    //{
+    //    BO.Product ourProduct = (Product)ProductListview.SelectedItem;
+    //   // if (ourProduct != null) { new ProductWindow().Show(); }
+    //}
 }
--- PL/ProductWindow.xaml.cs
+++ PL/ProductWindow.xaml.cs
@@ -16,160 +16,164 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using BO;
+using DocumentFormat.OpenXml.Wordprocessing;
 
-namespace PL
+namespace PL;
+
+/// <summary>
+/// Interaction logic for ProductWindow.xaml
+/// </summary>
+public partial class ProductWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for ProductWindow.xaml
-    /// </summary>
-    public partial class ProductWindow : Window
+    IBL bl = new Bl();
+    public ProductWindow(Button button)
     {
-        IBL bl = new Bl();
-        public ProductWindow()
-        {
-            InitializeComponent();
-            categoryBox.ItemsSource = Enum.GetValues(typeof(DO.Category));
-
-            //bl.Product.AddProduct(1, "", 1, BO.Category.shoes, 1);
-        }
-        ProductWindow(IBL bl, int id)
-        {
-
-
-        }
-        //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        //{
-
-        //}
-
-        private void categoryBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void id_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            ///Only numbers..
-            if( e.Key != Key.D0 && 
-                e.Key != Key.D1 && 
-                e.Key != Key.D2 &&
-                e.Key != Key.D3 && 
-                e.Key != Key.D4 &&
-                e.Key != Key.D5 &&
-                e.Key != Key.D6 &&
-                e.Key != Key.D7 && 
-                e.Key != Key.D8 &&
-                e.Key != Key.D9 && 
-                e.Key != Key.Back && 
-                e.Key != Key.NumPad0 && 
-                e.Key != Key.NumPad1 && 
-                e.Key != Key.NumPad2 && 
-                e.Key != Key.NumPad3 && 
-                e.Key != Key.NumPad4 && 
-                e.Key != Key.NumPad5 && 
-                e.Key != Key.NumPad6 && 
-                e.Key != Key.NumPad7 && 
-                e.Key != Key.NumPad8 && 
-                e.Key != Key.NumPad9 && 
-                e.Key != Key.Delete && 
-                e.Key != Key.Right && 
-                e.Key != Key.Left)
+        InitializeComponent();
+        categoryBox.ItemsSource = Enum.GetValues(typeof(DO.Category));
+        if (button == AddButton)
+            UpdateButton.Visibility = Visibility.Hidden;
+        else
+            AddButton.Visibility = Visibility.Hidden;
+            
+        //bl.Product.AddProduct(1, "", 1, BO.Category.shoes, 1);
+    }
+    ProductWindow(IBL bl, int id)
     {
-                e.Handled = true;
-            }
 
+
+    }
+    //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    //{
+
+    //}
+
+    private void categoryBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private void id_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        ///Only numbers..
+        if( e.Key != Key.D0 && 
+            e.Key != Key.D1 && 
+            e.Key != Key.D2 &&
+            e.Key != Key.D3 && 
+            e.Key != Key.D4 &&
+            e.Key != Key.D5 &&
+            e.Key != Key.D6 &&
+            e.Key != Key.D7 && 
+            e.Key != Key.D8 &&
+            e.Key != Key.D9 && 
+            e.Key != Key.Back && 
+            e.Key != Key.NumPad0 && 
+            e.Key != Key.NumPad1 && 
+            e.Key != Key.NumPad2 && 
+            e.Key != Key.NumPad3 && 
+            e.Key != Key.NumPad4 && 
+            e.Key != Key.NumPad5 && 
+            e.Key != Key.NumPad6 && 
+            e.Key != Key.NumPad7 && 
+            e.Key != Key.NumPad8 && 
+            e.Key != Key.NumPad9 && 
+            e.Key != Key.Delete && 
+            e.Key != Key.Right && 
+            e.Key != Key.Left)
+{
+            e.Handled = true;
         }
-        private void id_PreviewKeyDown1(object sender, KeyEventArgs e)
-        {
-            ///Only numbers..
-            if (e.Key != Key.D0 &&
-                e.Key != Key.D1 &&
-                e.Key != Key.D2 &&
-                e.Key != Key.D3 &&
-                e.Key != Key.D4 &&
-                e.Key != Key.D5 &&
-                e.Key != Key.D6 &&
-                e.Key != Key.D7 &&
-                e.Key != Key.D8 &&
-                e.Key != Key.D9 &&
-                e.Key != Key.Back &&
-                e.Key != Key.NumPad0 &&
-                e.Key != Key.NumPad1 &&
-                e.Key != Key.NumPad2 &&
-                e.Key != Key.NumPad3 &&
-                e.Key != Key.NumPad4 &&
-                e.Key != Key.NumPad5 &&
-                e.Key != Key.NumPad6 &&
-                e.Key != Key.NumPad7 &&
-                e.Key != Key.NumPad8 &&
-                e.Key != Key.NumPad9 &&
-                e.Key != Key.Delete &&
-                e.Key != Key.Right &&
-                e.Key != Key.Left  &&
-                e.Key != Key.Decimal)
-            {
-                e.Handled = true;
-            }
 
+    }
+    private void id_PreviewKeyDown1(object sender, KeyEventArgs e)
+    {
+        ///Only numbers..
+        if (e.Key != Key.D0 &&
+            e.Key != Key.D1 &&
+            e.Key != Key.D2 &&
+            e.Key != Key.D3 &&
+            e.Key != Key.D4 &&
+            e.Key != Key.D5 &&
+            e.Key != Key.D6 &&
+            e.Key != Key.D7 &&
+            e.Key != Key.D8 &&
+            e.Key != Key.D9 &&
+            e.Key != Key.Back &&
+            e.Key != Key.NumPad0 &&
+            e.Key != Key.NumPad1 &&
+            e.Key != Key.NumPad2 &&
+            e.Key != Key.NumPad3 &&
+            e.Key != Key.NumPad4 &&
+            e.Key != Key.NumPad5 &&
+            e.Key != Key.NumPad6 &&
+            e.Key != Key.NumPad7 &&
+            e.Key != Key.NumPad8 &&
+            e.Key != Key.NumPad9 &&
+            e.Key != Key.Delete &&
+            e.Key != Key.Right &&
+            e.Key != Key.Left  &&
+            e.Key != Key.Decimal)
+        {
+            e.Handled = true;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        var bc = new BrushConverter();
+
+        if (!int.TryParse(idtxt.Text, out int id))
         {
-            var bc = new BrushConverter();
+            idtxtMsg.Background = (Brush)bc.ConvertFrom("#DD4A48")!;
+            idtxtMsg.Content = "Enter a integer !";
+        }
+      //  if (id < 0) { txtBoxId.Background = (Brush)bc.ConvertFrom("#DD4A48"); txtBoxId.Text = "Positive integer only !"; }
+        if (!double.TryParse(price.Text, out double priceInt))
+        {
 
-            if (!int.TryParse(idtxt.Text, out int id))
+            priceMsg.Background = (Brush)bc.ConvertFrom("#DD4A48")!;
+            priceMsg.Content = "Enter a Double !";
+        }
+        if (!int.TryParse(inStock.Text, out int InStock))
+        {
+            inStockMsg.Background = (Brush)bc.ConvertFrom("#DD4A48")!;
+            inStockMsg.Content = "Enter a integer !";
+        }
+        if(categoryBox.Text.ToString()==BO.Category.all.ToString() || categoryBox.Text =="")
+        {
+            categoryBoxMsg.Background = (Brush)bc.ConvertFrom("#DD4A48")!;
+            categoryBoxMsg.Content = "Enter a Category except all !";
+        }
+        else
+        {
+            try
             {
-                idtxtMsg.Background = (Brush)bc.ConvertFrom("#DD4A48")!;
-                idtxtMsg.Content = "Enter a integer !";
-            }
-          //  if (id < 0) { txtBoxId.Background = (Brush)bc.ConvertFrom("#DD4A48"); txtBoxId.Text = "Positive integer only !"; }
-            if (!double.TryParse(price.Text, out double priceInt))
-            {
+                bl.Product.AddProduct(id, name.Text, priceInt, (BO.Category)categoryBox.SelectedItem, InStock);
+                this.Close(); Window win = Application.Current.Windows.OfType<Window>().SingleOrDefault(w => w.Name == "ProductListWindow")!;
+                if (win != null) { win.Close(); }
+                new ProductListWindow().Show();
+                    //.CustomerList(bl).Show();
 
-                priceMsg.Background = (Brush)bc.ConvertFrom("#DD4A48")!;
-                priceMsg.Content = "Enter a Double !";
             }
-            if (!int.TryParse(inStock.Text, out int InStock))
+            catch (AggregateException ex)
             {
-                inStockMsg.Background = (Brush)bc.ConvertFrom("#DD4A48")!;
-                inStockMsg.Content = "Enter a integer !";
-            }
-            if(categoryBox.Text.ToString()==BO.Category.all.ToString() || categoryBox.Text =="")
-            {
-                categoryBoxMsg.Background = (Brush)bc.ConvertFrom("#DD4A48")!;
-                categoryBoxMsg.Content = "Enter a Category except all !";
-            }
-            else
-            {
-                try
+                foreach (Exception innerException in ex.InnerExceptions)
                 {
-                    bl.Product.AddProduct(id, name.Text, priceInt, (BO.Category)categoryBox.SelectedItem, InStock);
-                    this.Close(); Window win = Application.Current.Windows.OfType<Window>().SingleOrDefault(w => w.Name == "ProductListWindow")!;
-                    if (win != null) { win.Close(); }
-                    new ProductListWindow().Show();
-                        //.CustomerList(bl).Show();
-
-                }
-                catch (AggregateException ex)
-                {
-                    foreach (Exception innerException in ex.InnerExceptions)
-                    {
-                        if (innerException.GetType() == typeof(IdAlreadyExistException)) { idtxt.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#DD4A48")!; }
-                        MessageBox.Show(innerException.ToString());
-                    }
+                    if (innerException.GetType() == typeof(IdAlreadyExistException)) { idtxt.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#DD4A48")!; }
+                    MessageBox.Show(innerException.ToString());
                 }
             }
         }
+    }
 
-        private void name_TextChanged(object sender, TextChangedEventArgs e)
+    private void name_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (name.Text == "") return;
+        if (!System.Text.RegularExpressions.Regex.IsMatch(name.Text, "^[a-zA-Z ]"))
         {
-            if (name.Text == "") return;
-            if (!System.Text.RegularExpressions.Regex.IsMatch(name.Text, "^[a-zA-Z ]"))
-            {
-                MessageBox.Show("This textbox accepts only alphabetical characters");
-                name.Text.Remove(name.Text.Length - 1);
-            }
-
+            MessageBox.Show("This textbox accepts only alphabetical characters");
+            name.Text.Remove(name.Text.Length - 1);
         }
+
     }
 }
